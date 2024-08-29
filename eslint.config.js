5b5a245c8a29da85d6e3b02fd9d63878d3d695cc
Claudio Tomas <ccaducarlos@gmail.com>
2024-08-29 17:10:57 +0200

    style: add profile style

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import rocketseatReact from '@rocketseat/eslint-config/react'
 
 export default [
   { ignores: ['dist'] },
@@ -28,6 +29,7 @@ export default [
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
+      ...rocketseatReact.rules,
       'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': [
         'warn',
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 
 import { Header } from './components/header'
 
-
+import styles from './App.module.css'
+import { SideBar } from './components/sidebar'
 export function App() {
 
   return (
+    <>
+      <Header />
+      <div className={styles.wrapper}>
+        <SideBar/>
+        <main>post</main>
+      </div>
+    </>
 
-    <Header />
 
   )
 }
