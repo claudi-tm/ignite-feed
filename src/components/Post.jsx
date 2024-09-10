e6dab0b88bef83f194d2cd3605a123002cb250a5
Claudio Tomas <ccaducarlos@gmail.com>
2024-09-10 18:59:45 +0200

    date format

---
+++ src/components/Post.jsx
@@ -0,0 +1,54 @@
+import { format, formatDistanceToNow } from 'date-fns';
+import { Avatar } from './Avatar';
+import { Comment } from './Comment';
+
+import { ptBR } from 'date-fns/locale';
+import styles from './Post.module.css';
+
+export function Post({ author, publishedAt, content }) {
+
+  const publishedAtFormated = format(publishedAt, "dd 'de' MMMM yyyy", { locale: ptBR })
+  const publishedDescription = formatDistanceToNow(publishedAt, { locale: ptBR, addSuffix: true })
+
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <Avatar src="https://github.com/diego3g.png" />
+          <div className={styles.authorInfo}>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
+          </div>
+        </div>
+
+        <time title={publishedAtFormated} dateTime={publishedAt.toISOString()}>{publishedDescription}</time>
+      </header>
+
+      <div className={styles.content}>
+        {content.map(line => {
+          if (line.type === 'paragraph') {return <p>{line.content}</p>}
+          else if (line.type === 'link') return <p><a href="">{line.content}</a></p>
+        })}
+
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea
+          placeholder="Deixe um comentário"
+        />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
+    </article>
+  )
+}
